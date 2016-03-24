3989d4b23547f9ab06befc22bc1f6b7db14272ca
Okke Formsma <okke@formsma.nl>
2016-03-24 14:01:54 +0100

    Added little test http server

---
+++ spongebob/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Path[1:]
+	for i := 0; i < 10; i++ {
+    	fmt.Fprintf(w, "He lives in a pineapple under the sea ... %s!\n", r.URL.Path[1:])
+    	if name == "Spongebob Squarepants" {
+    		fmt.Fprintf(w, "Absorbant and yellow and porous is he!\n")
+    	}
+    }
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    fmt.Print("Yup")
+    http.ListenAndServe(":8080", nil)
+}
